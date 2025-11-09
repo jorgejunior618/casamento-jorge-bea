1c813f73c8ddba89dccf5fa87a005accbe7f55d3
Jorge Junior <56480032+jorgejunior618@users.noreply.github.com>
2025-11-09 13:56:21 -0300

    feat: componentização da lista de presente

--- src/app/lista-de-presentes/page.tsx
+++ src/app/lista-de-presentes/page.tsx
@@ -1,52 +1,11 @@
-"use client";
-import ItemPresente from "@/components/views/ItemPresente";
-import listaPresentes from "@/mocks/lista-presentes";
+import Rodape from "@/components/Rodape";
+import ViewListaPresentes from "@/views/ListaPresentes";
 
 export default function ListaPresentes() {
   return (
-    <div>
-      <header
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          backgroundColor: "#ffcbbd",
-        }}
-      >
-        <div style={{ width: "1080px" }}>
-          <h1>Lista de presentes</h1>
-          <h2>Contagem regressiva: 2 meses e 13 dias</h2>
-          <div
-            style={{
-              width: "90px",
-              height: "120px",
-              border: "10px solid white",
-              borderBottom: "35px solid white",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <span>Foto</span>
-            <span>Bonita</span>
-          </div>
-        </div>
-      </header>
-      <main>
-        <ul
-          style={{
-            marginTop: "30px",
-            display: "flex",
-            gap: "12px",
-            flexWrap: "wrap",
-            justifyContent: "center",
-          }}
-        >
-          {listaPresentes.map((presente, idx) => (
-            <ItemPresente key={idx} presente={presente} />
-          ))}
-        </ul>
-      </main>
-    </div>
+    <>
+      <ViewListaPresentes />
+      <Rodape />
+    </>
   );
 }
---
+++ src/views/ListaPresentes/index.tsx
@@ -0,0 +1,102 @@
+"use client";
+import ItemPresente from "@/components/ItemPresente/ItemPresente";
+import listaPresentes from "@/mocks/lista-presentes";
+import styles from "./styles.module.scss";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import LinkButton from "@/components/LinkButton";
+import VoltarIcon from "@/assets/voltar-icon.svg";
+import ModalConfirmarCompra from "@/components/ModalConfirmarCompra";
+import { useState } from "react";
+import ModalConfirmacao from "@/components/ModalConfirmarcao";
+
+interface DadosModal {
+  aberto: boolean;
+  presenteID: string;
+  comprador: string;
+}
+export default function ViewListaPresentes({
+  estaLogado = false,
+}: {
+  estaLogado?: boolean;
+}) {
+  const redirecionador = useRouter();
+  const [dadosModal, setDadosModal] = useState<DadosModal>({
+    aberto: false,
+    presenteID: "",
+    comprador: "",
+  });
+  const [modalConfirmacaoAberto, setConfirmacaoAberto] =
+    useState<boolean>(false);
+
+  const abrirModalConfirmacao = (presenteID: string) => {
+    setDadosModal({ aberto: true, presenteID, comprador: "" });
+  };
+
+  const perguntarConfirmacao = (presenteID: string, comprador: string) => {
+    setDadosModal({ aberto: true, presenteID, comprador });
+    setConfirmacaoAberto(true);
+  };
+  const confirmarConfirmacao = () => {
+    const presenteSelecionado = listaPresentes.findIndex(
+      (pres) => pres.id === dadosModal.presenteID
+    );
+
+    if (presenteSelecionado === -1)
+      return alert("Ocorreu um erro, tente novamente mais tarde");
+
+    listaPresentes[presenteSelecionado] = {
+      ...listaPresentes[presenteSelecionado],
+      confirmado: true,
+      comprador: dadosModal.comprador,
+    };
+    setDadosModal({ aberto: false, presenteID: "", comprador: "" });
+    setConfirmacaoAberto(false);
+  };
+  const cancelarConfirmacao = () => {
+    setDadosModal({ aberto: false, presenteID: "", comprador: "" });
+  };
+  const voltarConfirmacao = () => {
+    setConfirmacaoAberto(false);
+  };
+
+  const redirecionarHome = () => {
+    redirecionador.push("/sobre-o-casamento");
+  };
+  return (
+    <main className="container">
+      <div className={styles.Pagina}>
+        <LinkButton type="texto" onClick={redirecionarHome}>
+          <div className="BotaoVoltar">
+            <Image src={VoltarIcon} alt="voltar" width={14} height={14} />{" "}
+            <span>VOLTAR</span>
+          </div>
+        </LinkButton>
+        <h1 className={styles.TituloLista}>Lista de presentes</h1>
+        <ul className={styles.ListaPresentes}>
+          {listaPresentes
+            .filter((presente) => (estaLogado ? true : !presente.confirmado))
+            .map((presente, idx) => (
+              <ItemPresente
+                key={idx}
+                presente={presente}
+                estaLogado={estaLogado}
+                adicionarComprador={abrirModalConfirmacao}
+              />
+            ))}
+        </ul>
+      </div>
+      <ModalConfirmarCompra
+        aberto={dadosModal.aberto}
+        presenteID={dadosModal.presenteID}
+        confirmar={perguntarConfirmacao}
+        cancelar={cancelarConfirmacao}
+      />
+      <ModalConfirmacao
+        aberto={modalConfirmacaoAberto}
+        confirmar={confirmarConfirmacao}
+        cancelar={voltarConfirmacao}
+      />
+    </main>
+  );
+}
